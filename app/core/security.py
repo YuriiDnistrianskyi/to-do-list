a66f8870567b5a1d30bbcdba441353061f4524e4
YuraDnistriansky <yura.dniester@gmail.com>
2026-08-07 12:59:27 +0300

    Update security.py

--- app/core/security.py
+++ app/core/security.py
@@ -1,2 +1,15 @@
+from passlib.context import CryptContext
+import hashlib
+
+pwd_context = CryptContext(
+    schemes=['argon2'],
+    deprecated='auto'
+)
+
+
 def create_hash(password: str) -> str:
-    pass
+    hashed = pwd_context.hash(password)
+    return hashed
+
+def verify_password(password_hash: str, password: str) -> bool:
+    return pwd_context.verify(password, password_hash)
