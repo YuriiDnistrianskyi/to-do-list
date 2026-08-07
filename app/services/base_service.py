e2c5e9c012139320ba27c630285f97d862405c1d
YuraDnistriansky <yura.dniester@gmail.com>
2026-08-07 11:36:39 +0300

    Create base_service.py

---
+++ app/services/base_service.py
@@ -0,0 +1,32 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from typing import Generic, TypeVar
+
+from app.repositories.base_repository import BaseRepository
+
+
+T = TypeVar("T")
+
+class BaseService(Generic[T]):
+
+    def __init__(self, repository: BaseRepository[T]):
+        self.repository = repository
+
+
+    async def get_all(self, session: AsyncSession) -> list[T]:
+        return await self.repository.get_all(session)
+
+
+    async def get_by_id(self, obj_id: int, session: AsyncSession) -> T:
+        return await self.repository.get_by_id(obj_id, session)
+
+
+    async def create(self, schema, session: AsyncSession) -> T:
+        pass
+
+
+    async def update(self, obj_id: int, schema, session: AsyncSession) -> T:
+        pass
+
+
+    async def delete(self, obj_id: int, session: AsyncSession) -> None:
+        await self.repository.delete(obj_id, session)
