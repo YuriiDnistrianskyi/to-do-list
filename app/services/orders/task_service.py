f43ac33729db495939158b2b172c2fbd7126e3e8
YuraDnistriansky <yura.dniester@gmail.com>
2026-08-07 12:26:00 +0300

    update task service

--- app/services/orders/task_service.py
+++ app/services/orders/task_service.py
@@ -1,8 +1,10 @@
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import Boolean
 
 from app.services.base_service import BaseService
 from app.database.models.task import Task
 from app.schemas.task_schemas import CreateTaskSchema, UpdateTaskSchema
+from app.exceptions.task_already_completed import TaskAlreadyCompleted
 
 
 class TaskService(BaseService[Task]):
@@ -28,4 +30,11 @@ class TaskService(BaseService[Task]):
         if 'deadline' in data:
             obj.deadline = data['deadline']
 
+        return obj
+
+    async def complete(self, obj_id: int, session: AsyncSession) -> Task:
+        obj = await self.repository.get_by_id(obj_id, session)
+        if obj.is_completed:
+            raise TaskAlreadyCompleted("Task already completed")
+        obj.is_completed = Boolean(True)
         return obj
