7eb1bc97177596c5c9780f532693e9cfe0b427a4
YuraDnistriansky <yura.dniester@gmail.com>
2026-08-07 16:17:14 +0300

    create email service

--- app/core/config.py
+++ app/core/config.py
@@ -5,3 +5,9 @@ load_dotenv()
 
 DATABASE_URL = getenv("DATABASE_URL")
 SECRET_JWT_KEY = getenv("SECRET_JWT_KEY")
+
+SMTP_HOST = getenv("SMTP_HOST")
+SMTP_PORT = getenv("SMTP_PORT")
+SMTP_USERNAME = getenv("SMTP_USERNAME")
+SMTP_PASSWORD = getenv("SMTP_PASSWORD")
+SMTP_FROM = getenv("SMTP_FROM")
